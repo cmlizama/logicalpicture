743e4a1869da6bd066dce5fac5daf29ebc0329df
Chris Lizama <christopher.lizama@gmail.com>
2023-12-28 10:13:48 -0600

    add rss (technically Atom) feed

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,11 @@
 const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
+const pluginRss = require("@11ty/eleventy-plugin-rss");
 
 module.exports = eleventyConfig => {
   // Use the eleventyConfig object's built in methods to customize
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy("assets/**/**");
+  eleventyConfig.addPlugin(pluginRss);
 
   return {
     dir: {
---
+++ _data/_metadata.js
@@ -0,0 +1,9 @@
+{
+	"feed": {
+		"subtitle": "Thoughts on Wildlife, Meditation, and Crafting",
+		"filename": "feed.xml",
+		"path": "/feed/feed.xml",
+		"url": "https://bogflowers.com/feed/feed.xml",
+		"id": "https://bogflowers.com"
+	},
+},
